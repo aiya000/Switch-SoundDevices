9bb662826616078341cf7ed8834c9291c57fe051
aiya000 <aiya000.develop@gmail.com>
2021-07-27 05:26:50 +0900

    :+1: Make Setup.ps1

---
+++ Setup.ps1
@@ -0,0 +1,6 @@
+New-Item "$($profile | split-path)\Modules\AudioDeviceCmdlets" -Type directory -Force
+Copy-Item ".\AudioDeviceCmdlets/AudioDeviceCmdlets.dll" "$($profile | split-path)\Modules\AudioDeviceCmdlets\AudioDeviceCmdlets.dll"
+Set-Location "$($profile | Split-Path)\Modules\AudioDeviceCmdlets"
+Get-ChildItem | Unblock-File
+Import-Module AudioDeviceCmdlets
+Get-Module
